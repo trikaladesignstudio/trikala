74910c9f8007b0e2eeb6e5064f2090c2bac18ed4
rishi23root <rishi23root@gmail.com>
2024-10-03 22:15:23 +0530

    added layout base

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,18 +3,19 @@ import Section from "@/components/custom/Section";
 export default function Home() {
   return (
     <>
-    <nav></nav>
-    <main>
-      <Section >
-        <h1 className="text-3xl border-2 border-red-200">hekko</h1>
+      <nav></nav>
+      <main>
+        <Section className="flex-row">
+          <h1 className="border-2 border-red-200 w-full flex-1 flex justify-center items-center align-middle">hekko</h1>
+          <h1 className="border-2 border-red-200 w-full flex-1 flex justify-center items-center align-middle">hekko</h1>
+          <h1 className="border-2 border-red-200 w-full flex-1 flex justify-center items-center align-middle">hekko</h1>
+        </Section>
+        <Section >
+          <h1 className="text-3xl border-2 border-red-200 w-full flex-1">hekko</h1>
 
-      </Section>
-
-      <Section >
-        <h1>hekko</h1>
-
-      </Section>
-    </main>
+          <h1 className="text-3xl border-2 border-red-200 w-full flex-1">hekko</h1>
+        </Section>
+      </main>
     </>
   );
 }
--- src/components/custom/Section.tsx
+++ src/components/custom/Section.tsx
@@ -1,14 +1,14 @@
 import { cn } from "@/lib/utils";
 import React, { ReactNode } from "react";
 
-type Section= {
+type Section = {
   className?: string;
   children: ReactNode;
 }
 
 function Section({ className, children }: Section) {
   return (
-    <section className={cn("min-h-screen px-8 py-12", className)}>
+    <section className={cn("min-h-screen px-[2rem] lg:px-[10rem] py-12 flex flex-col ", className)}>
       {children}
     </section>
   );
