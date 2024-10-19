876b3b0cac89d55c031cb5b78253e598900acb69
rishi23root <rishi23root@gmail.com>
2024-10-19 17:43:42 +0530

    updated form logic, snap section with framer motion

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,37 +1,42 @@
+"use client";
 import Footer from "@/components/custom/Footer";
 import Interior from "@/components/custom/Interior";
 import Lead from "@/components/custom/Lead";
 import SectionGrid from "@/components/custom/SectionGrid";
 import Testimonials from "@/components/custom/Testimonials";
 import Working from "@/components/custom/Working";
+import { AnimatePresence } from "framer-motion";
 
 export default function Home() {
   return (
     <>
       {/* <div className="absolute top-0 left-0 w-full h-full"> */}
-      <main className="relative flex flex-col snap-y snap-mandatory h-screen overflow-y-scroll overflow-hidden border-2">
-        <nav className="snap-start">tjos</nav>
-        <SectionGrid className="grid-rows-3 grid-cols-3 gap-4">
-          <div className="row-span-1 col-span-3  flex">
-            <h1 className="text-4xl lg:text-9xl  w-full flex-1 flex justify-center items-end align-bottom">
-              Trikala Studios
-            </h1>
-          </div>
-          <div className="col-span-3">
-            <h1 className="text-1xl lg:text-3xl w-full flex-1 flex justify-center items-center align-middle">
-              comming soon :)
-            </h1>
-          </div>
-        </SectionGrid>
-        {/* <Section >
+      <main className="relative flex flex-col snap-y snap-mandatory h-screen overflow-y-scroll overflow-hidden">
+        <AnimatePresence mode="wait" initial={false} >
+          <nav className="snap-start">tjos</nav>
+          <SectionGrid className="grid-rows-3 grid-cols-3 gap-4">
+            <div className="row-span-1 col-span-3  flex">
+              <h1 className="text-4xl lg:text-9xl  w-full flex-1 flex justify-center items-end align-bottom">
+                Trikala Studios
+              </h1>
+            </div>
+            <div className="col-span-3">
+              <h1 className="text-1xl lg:text-3xl w-full flex-1 flex justify-center items-center align-middle">
+                comming soon :)
+              </h1>
+            </div>
+          </SectionGrid>
+          {/* <Section >
           <h1 className="text-3xl  w-full flex-1">hekko</h1>
           
           <h1 className="text-3xl  w-full flex-1">hekko</h1>
         </Section> */}
-        <Lead/>
-        <Working/>
-        <Interior/>
-        <Testimonials/>
+          <Lead />
+          <Working />
+          <Interior />
+          <Testimonials />
+          <Footer />
+        </AnimatePresence>
       </main>
       {/* </div> */}
     </>
