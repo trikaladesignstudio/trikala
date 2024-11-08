c0cb7c052dc30e71955590294f6cacd179b9afc6
Nishant4coding <srivastava4nishant@gmail.com>
2024-11-08 20:44:04 +0530

    expertise  section finalize with images

--- src/components/custom/Expertise.tsx
+++ src/components/custom/Expertise.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useState, useEffect, useMemo } from "react";
+import React, { useState, useMemo } from "react";
 import Sections from "./Section";
 import Image, { StaticImageData } from "next/image";
 import { expertise } from "../../constants/index";
@@ -26,20 +26,13 @@ const CarouselItemComponent: React.FC<{
     <Image
       src={item.image}
       alt={item.alt}
-      className="lg:w-[20vw] lg:h-[60vh] h-[30vh] w-auto object-cover rounded-full border-2"
+      className="lg:w-[20vw] lg:h-[70vh] h-[30vh] w-auto object-cover rounded-full border-2"
     />
   </div>
 );
 
 const Expertise: React.FC<CarouselProps> = ({ items = expertise }) => {
   const [activeIndex, setActiveIndex] = useState(0);
-  const [isMobile, setIsMobile] = useState(window.innerWidth < 768);
-
-  useEffect(() => {
-    const handleResize = () => setIsMobile(window.innerWidth < 768);
-    window.addEventListener("resize", handleResize);
-    return () => window.removeEventListener("resize", handleResize);
-  }, []);
 
   const handleClick = (index: number) => {
     setActiveIndex(index);
@@ -47,38 +40,20 @@ const Expertise: React.FC<CarouselProps> = ({ items = expertise }) => {
 
   const visibleItems = useMemo(() => {
     const totalItems = items.length;
-    const positions = isMobile
-      ? [activeIndex - 1, activeIndex, activeIndex + 1]
-      : [
-          activeIndex - 2,
-          activeIndex - 1,
-          activeIndex,
-          activeIndex + 1,
-          activeIndex + 2,
-        ];
-
-    const scales = isMobile
-      ? ["scale-75 z-0", "scale-100 z-10", "scale-75 z-0"]
-      : [
-          "scale-50 z-0",
-          "scale-75 z-0",
-          "scale-100 z-10",
-          "scale-75 z-0",
-          "scale-50 z-0",
-        ];
-
+    const positions = [activeIndex - 1, activeIndex, activeIndex + 1];
+    const scales = ["scale-75 z-0", "scale-100 z-10", "scale-75 z-0"];
     return positions.map((pos, idx) => ({
       index: (pos + totalItems) % totalItems,
       ...items[(pos + totalItems) % totalItems],
       position: scales[idx],
     }));
-  }, [activeIndex, items, isMobile]);
+  }, [activeIndex, items]);
 
   return (
     <Sections className="lg:px-0 px-0 fcc w-full  gap-4 lg:min-h-fit">
       <Heading className="text-center">Expertise</Heading>
       <div className="relative flex justify-center w-full">
-        <div className="flex w-full justify-between items-center transition-transform duration-500 ease-in-out">
+        <div className="flex w-full justify-center items-center transition-transform duration-500 ease-in-out">
           {visibleItems.map((item) => (
             <CarouselItemComponent
               key={item.index}
