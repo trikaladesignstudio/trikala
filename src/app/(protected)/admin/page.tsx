88c8324cdfa9781327324fb325e3fba30d22f681
rishi23root <rishi23root@gmail.com>
2024-12-16 21:13:01 +0530

    fix admin snap

--- src/app/(protected)/admin/page.tsx
+++ src/app/(protected)/admin/page.tsx
@@ -1,5 +1,6 @@
 import PathHeading from "@/components/custom/PathHeading";
 import ProjectShowcase from "@/components/custom/projectShow";
+import Section from "@/components/custom/Section";
 import LogoutBtn from "@/components/user/Logout";
 import { allSections } from "@/utils/client_utils";
 import { getAllProjects } from "@/utils/dbActions";
@@ -11,7 +12,7 @@ export default async function Admin() {
   const projects = await getAllProjects();
 
   return (
-    <div className="container mx-auto px-4 py-8 flex flex-col gap-4">
+    <Section className="container mx-auto px-4 py-8 flex flex-col gap-4">
       <div className="flex justify-between items-center mb-8">
         <PathHeading />
         <div className="flex gap-4">
@@ -37,6 +38,6 @@ export default async function Admin() {
           />
         </div>
       ))}
-    </div>
+    </Section>
   );
 }
