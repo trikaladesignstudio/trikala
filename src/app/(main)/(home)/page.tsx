307b0107ce0a0c794d80008446e053b4d029f33f
rishi23root <rishi23root@gmail.com>
2024-12-26 10:20:15 +0530

    using ssr when needed

--- src/app/(main)/(home)/page.tsx
+++ src/app/(main)/(home)/page.tsx
@@ -8,19 +8,19 @@ import dynamic from "next/dynamic";
 import { Suspense } from "react";
 
 const Expertise = dynamic(() => import("@/components/sections/ExpertiseTest"), {
-  ssr: false,
+  ssr: true,
   suspense: true,
 });
 
 const Featured = dynamic(() => import("@/components/sections/Feature"), {
-  ssr: false,
+  ssr: true,
   suspense: true,
 });
 
 const PriceEstimator = dynamic(
   () => import("@/components/sections/PriceEstimatorTest"),
   {
-    ssr: false,
+    ssr: true,
     suspense: true,
   }
 );
@@ -28,20 +28,22 @@ const PriceEstimator = dynamic(
 const Testimonials = dynamic(
   () => import("@/components/sections/Testimonials"),
   {
-    ssr: false,
+    ssr: true,
     suspense: true,
   }
 );
 
 const Video = dynamic(() => import("@/components/sections/Video"), {
-  ssr: false,
+  ssr: true,
   suspense: true,
 });
 
 const Working = dynamic(() => import("@/components/sections/Working"), {
-  ssr: false,
+  ssr: true,
   suspense: true,
 });
+
+export const revalidate = 300;
 
 export default async function Home() {
   const heroData = filterAllProjects(sectionType.hero);
