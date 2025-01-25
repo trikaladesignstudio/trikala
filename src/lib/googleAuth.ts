38d9792ed8a23849a4f0f6f156a0f894ee94d22a
rishi23root <rishi23root@gmail.com>
2025-01-26 01:21:28 +0530

    updated for google auth

--- src/lib/googleAuth.ts
+++ src/lib/googleAuth.ts
@@ -5,7 +5,16 @@ import { GoogleSpreadsheet } from "google-spreadsheet";
 const keyFileName = "./key.json";
 
 const serviceAccountAuth = new GoogleAuth({
-  keyFile: keyFileName,
+  credentials: {
+    type: "service_account",
+    project_id: "learned-acronym-448817-f9",
+    private_key_id: "653869b46a0b3afeff726cea39162622dcbb7412",
+    private_key: process.env.private_google_key,
+    client_email:
+      "sheet-reader@learned-acronym-448817-f9.iam.gserviceaccount.com",
+    client_id: "104664558320891307577",
+    universe_domain: "googleapis.com",
+  },
   scopes: ["https://www.googleapis.com/auth/spreadsheets"],
 });
 const dataDoc = new GoogleSpreadsheet(
